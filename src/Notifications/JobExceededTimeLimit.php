171b9a7ec9ffc2a5fe2904be42ac8ca469dec62e
anejjar Elhoucine <anejjarlhouciane@gmail.com>
2025-04-29 12:07:37 +0100

    Fix namespace declaration in JobExceededTimeLimit notification

--- src/Notifications/JobExceededTimeLimit.php
+++ src/Notifications/JobExceededTimeLimit.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace YourNamespace\JobMonitor\Notifications;
+namespace Custplace\JobMonitor\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
